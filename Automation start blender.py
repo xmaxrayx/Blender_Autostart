8f5d5b18c2c40b1d84a8182e265a4ed73890199d
xMaxrayx <101531362+xmaxrayx@users.noreply.github.com>
2023-03-31 05:28:54 +0200

    Update Automation start blender.py

--- Automation start blender.py
+++ Automation start blender.py
@@ -1,6 +1,6 @@
 #import os
 #result = os.system("\"C:/Program Files (x86)/Notepad++/notepad++.exe\"")
-#couse crash when notepad wasn't running
+#couse crash when notepad wasn't running at first before run the script   
 
 
 #result = subprocess.run(["C:/Program Files (x86)/Notepad++/notepad++.exe"], capture_output=True)
@@ -8,5 +8,27 @@
 #print(result.stderr)
 
 
+
+
+import bpy
+import time
 import subprocess
-result = subprocess.run(["C:/Program Files (x86)/Notepad++/notepad++.exe"])
+subprocess.run(["C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"], capture_output=False)
+
+
+
+
+subprocess.Popen([r"C:/Program Files (x86)/Notepad++/notepad++.exe"])
+
+
+
+
+
+
+#result = subprocess.run(["C:/Program Files (x86)/Notepad++/notepad++.exe"], capture_output=False)
+
+
+
+#result = subprocess.run("\"C:/Program Files (x86)/Notepad++/notepad++.exe\"")
+#time.sleep(1) #add it to pervent multi-spam running that can lead blender to crash 
+#never mind turn out it pervent to use blnder unless you close the other apps
